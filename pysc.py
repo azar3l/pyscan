815c8787aa7779044490c0fb783ed4ed620cfce2
Azrael <azrael@localhost.localdomain>
2015-11-12 21:03:45 +0530

    testing script

--- pysc.py
+++ pysc.py
@@ -1,22 +1,54 @@
-from socket import *
+#!/usr/bin/python
+import socket
+import subprocess
+import sys
+from datetime import datetime
 
-def port_scan(ip,start,end):
-open_ports=[]
-        scan_ip = gethostbyname(ip)
-        for i in range(start,end+1):
-print('Scanning Port ',i)
-                sock = socket(AF_INET,SOCK_STREAM)
-sock.settimeout(20)
-result=sock.connect_ex((scan_ip,i))
-if result == 0:
-open_ports.append(i)
-print("Following port(s) are open:")
-for i in open_ports:
-print(i)
-if(len(open_ports)==0):
-print("None!!")
+# Clear the screen
+subprocess.call('clear', shell=True)
 
-ip=raw_input("Enter ip or host name:")
-start,end=raw_input("Enter port range to scan(Eg. 50-100,50):").split('-')
-port_scan(ip,int(start),int(end))
+# Ask for input
+remoteServer    = raw_input("Enter a remote host to scan: ")
+remoteServerIP  = socket.gethostbyname(remoteServer)
+
+# Print a nice banner with information on which host we are about to scan
+print "-" * 60
+print "Please wait, scanning remote host", remoteServerIP
+print "-" * 60
+
+# Check what time the scan started
+t1 = datetime.now()
+
+# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
+
+# We also put in some error handling for catching errors
+
+try:
+        for port in range(1,1025):  
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                            result = sock.connect_ex((remoteServerIP, port))
+                                    if result == 0:
+                                                    print "Port {}: \t Open".format(port)
+                                                            sock.close()
+
+except KeyboardInterrupt:
+        print "You pressed Ctrl+C"
+            sys.exit()
+
+except socket.gaierror:
+        print 'Hostname could not be resolved. Exiting'
+            sys.exit()
+
+except socket.error:
+        print "Couldn't connect to server"
+            sys.exit()
+
+            # Checking the time again
+            t2 = datetime.now()
+
+            # Calculates the difference of time, to see how long it took to run the script
+            total =  t2 - t1
+
+            # Printing the information to screen
+            print 'Scanning Completed in: ', total
 
